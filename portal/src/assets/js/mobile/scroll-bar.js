54aa13169c2796955fb399e19511e2f7bf5ca0dc
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-11-10 17:49:19 +0100

    Make necessary changes

--- portal/src/assets/js/mobile/scroll-bar.js
+++ portal/src/assets/js/mobile/scroll-bar.js
@@ -4,13 +4,20 @@ $(function scrollBar() {
 	var mobileWrapperTxTable = $(".mobile-wrapper-tx-table");
 
 	scrollBarWrapper.css("width", mobileWrapperTxTable.find(".tx-table").width());
+	scrollBarWrapper.css("width", mobileWrapperTxTable.find(".stmt-table").width());
 	$(".scroll-bar-div").css("width", mobileWrapperTxTable.find(".general-table").width());
 	scrollBarWrapper.scroll(() => {
 		mobileWrapperTxTable.find(".tx-table")
+			.scrollLeft(scrollBarWrapper.scrollLeft());
+		mobileWrapperTxTable.find(".stmt-table")
 			.scrollLeft(scrollBarWrapper.scrollLeft());
 	});
 	mobileWrapperTxTable.find(".tx-table").scroll(() => {
 		scrollBarWrapper
 			.scrollLeft(mobileWrapperTxTable.find(".tx-table").scrollLeft());
+	});
+	mobileWrapperTxTable.find(".stmt-table").scroll(() => {
+		scrollBarWrapper
+			.scrollLeft(mobileWrapperTxTable.find(".stmt-table").scrollLeft());
 	});
 });
